fd1141dcc32849ba5c537ae0a459b9ae7e71f595
Fernando Marsicano Gomes <fmarsica@everis.com>
2020-10-26 00:07:20 -0300

    First Commit

---
+++ src/exercicioLogicaProgramaçao6/ExercicioSeis.java
@@ -0,0 +1,37 @@
+package exercicioLogicaProgramaçao6;
+import java.util.Scanner;
+
+public class ExercicioSeis {
+
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	     
+	    System.out.print(
+	      "Este programa permite calcular o MDC\n");
+	    System.out.print("Informe o primeiro número: ");
+	    int x = sc.nextInt();
+	 
+	    System.out.print("Informe o segundo número: ");
+	    int y = sc.nextInt();
+	 
+	    System.out.println("\nO Máximo Divisor Comum de "
+	      + x + " e " + y + " é " + mdc(x, y));
+	    
+	    sc.close();
+	  }
+	 
+	  public static int mdc(int a, int b){
+	    int resto;
+	 
+	    while(b != 0){
+	      resto = a % b;
+	      a = b;
+	      b = resto;
+	    }
+	 
+	    return a;
+	  }
+
+	}
+
+
